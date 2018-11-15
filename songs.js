fb63e5caa43af8599d5b82543ae17ad3eb600393
Unknown <ahassett@middlebury.edu>
2018-11-15 16:55:33 -0500

    Makes the bar histogram work

---
+++ songs.js
@@ -0,0 +1,61 @@
+const URL = "http://www.cs.middlebury.edu/~candrews/classes/cs465-f18/data/gloom_index.csv";
+const SVG = d3.select('#vis');
+
+const WIDTH = 500;
+const HEIGHT = 500;
+
+const margin = {
+  top: 10,
+  right: 30,
+  left: 20,
+  bottom: 20
+}
+
+SVG.attr("width", WIDTH + margin.left + margin.right)
+  .attr("height", HEIGHT + margin.top + margin.bottom);
+
+// gloom_index, valence, and pct_sad
+const cht_gloom  = SVG.append("g").attr("id", "gloom_index");
+const cht_val = SVG.append("g").attr("id", "valence");
+const cht_pctsad = SVG.append("g").attr("id", "pct_sad");
+
+cht_gloom.attr("transfrom", `translate(${margin.left}, ${margin.top})`);
+
+d3.csv(URL).then(function(data){
+    console.log(data);
+    const x_scale = d3.scaleLinear()
+      .domain([d3.min(data, (d) => d["gloom_index"]), d3.max(data, (d) => d["gloom_index"])])
+      .range([0, WIDTH]);
+
+    const y_scale = d3.scaleLinear()
+      .range([HEIGHT, 0]);
+
+    var histogram = d3.histogram()
+      .value((d) => d["gloom_index"])
+      .domain(x_scale.domain())
+      //.thresholds(x_scale.ticks(20));
+
+    var bins = histogram(data);
+    console.log(bins);
+
+    y_scale.domain([0, d3.max(bins, (d)=> d.length)]);
+
+    cht_gloom.selectAll("rect")
+      .data(bins)
+      .enter()
+      .append("rect")
+      .attr("x", (d, i) => x_scale(d.x0))
+      .attr("y", (d) => y_scale(d.length))
+      .attr("height", (d) => HEIGHT - y_scale(d.length))
+      .attr("width", WIDTH / bins.length)
+      .style("fill", "black")
+      .style("padding", 0);
+
+    cht_gloom.selectAll("text")
+      .data(bins)
+      .enter()
+      .append("text")
+      .attr("x", (d, i) => i * 10)
+      .attr("y", 550)
+      .text((d) => d.length)
+});
