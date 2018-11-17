b3550b017e391d6489916825c7bad5f3ad6616dd
Unknown <ahassett@middlebury.edu>
2018-11-17 08:47:13 -0500

    Brush is working!!!

--- songs.js
+++ songs.js
@@ -110,33 +110,36 @@ d3.csv(URL).then(function(data){
         */
         let brush = d3.brushX().extent([[0,0], [WIDTH, HEIGHT]]);
 
+
         brush.on("brush", function(d){
-
             let extent = d3.event.selection;
-            let x_extent = [extent[0], extent[1]].map(x_scale.invert);
+            if (extent != null){
+                let x_extent = [extent[0], extent[1]].map(x_scale.invert);
 
-            // "grey" the deselected columns
-            col.classed("deselected", function(d) {
-                return (d.x0 < d3.min(x_extent) || d.x1 > d3.max(x_extent))
-            });
-
-            // end brush
-            brush.on("end", function(){
-                if (d3.event.selection == null){
-                    col.classed("deselected", false);
-                }
-            });
-
-            /*
-            // take away previous brushes
-            brush.on("start", function(){
-                if (d3.event.sourceEvent.type === "mousedown") {
-                    d3.selectAll('.brush').call(brush.move, null);
-                    console.log("restart");
-                }
-            });*/
-
+                // "grey" the deselected columns
+                col.classed("deselected", function(d) {
+                    return (d.x0 < d3.min(x_extent) || d.x1 > d3.max(x_extent))
+                });
+            }
         });
+
+        // end brush
+        brush.on("end", function(){
+            if (d3.event.selection == null){
+                col.classed("deselected", false);
+            }
+        });
+
+
+        // take away previous brushes
+        brush.on("start", function(){
+            if (d3.event.sourceEvent.type === "mousedown") {
+                d3.selectAll('.col').classed('deselected', false); 
+                d3.selectAll('.brush').call(brush.move, null);
+                console.log("restart");
+            }
+        });
+
         // call brush
         CHARTS[colName].append("g").classed('brush', true).call(brush);
     }
